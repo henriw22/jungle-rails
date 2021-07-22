8145a19c838920275732fcad695c87b91b1792ee
henriw22 <henriw22@gmail.com>
2021-07-22 05:38:36 +0000

    added order details after checkout, UI change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,18 +3,18 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-
+  
   def create
     charge = perform_stripe_charge
     order  = create_order(charge)
-
+    
     if order.valid?
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
-
+    
   rescue Stripe::CardError => e
     redirect_to cart_path, flash: { error: e.message }
   end
